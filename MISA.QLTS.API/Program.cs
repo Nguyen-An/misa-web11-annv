37f8ed2779dccaabfa6d12c8482fc41233221834
An Nguyen <vanan6b@gmail.com>
2023-02-05 19:13:21 +0700

    upload code last time

--- MISA.QLTS.API/Program.cs
+++ MISA.QLTS.API/Program.cs
@@ -1,10 +1,30 @@
+using MISA.QLTS.BL;
+using MISA.QLTS.BL.AssetCategoryBL;
+using MISA.QLTS.DL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped(typeof(IBaseDL<>), typeof(BaseDL<>));
+builder.Services.AddScoped(typeof(IBaseBL<>), typeof(BaseBL<>));
+
+builder.Services.AddScoped<IAssetDL, AssetDL>();
+builder.Services.AddScoped<IAssetBL, AssetBL>();
+
+builder.Services.AddScoped<IAssetCategoryDL, AssetCategoryDL>();
+builder.Services.AddScoped<IAssetCategoryBL, AssetCategoryBL>();
+
+builder.Services.AddScoped<IDepartmentDL, DepartmentDL>(); 
+builder.Services.AddScoped<IDepartmentBL, DepartmentBL>();
+
+
 
 builder.Services.AddControllers().ConfigureApiBehaviorOptions(options => 
     options.SuppressModelStateInvalidFilter = true
 );
+
+DatabaseContext.ConnectionString = builder.Configuration.GetConnectionString("MySql");
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
